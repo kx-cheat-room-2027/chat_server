f9261a093cf651bfb017e1d5161fa25014741a1f
hasya <Nedio7@qq.com>
2026-04-23 13:45:19 +0800

    登录/注册接口放行

--- src/main/java/com/example/chat_server/filter/AuthenticationTokenFilter.java
+++ src/main/java/com/example/chat_server/filter/AuthenticationTokenFilter.java
@@ -1,7 +1,10 @@
 package com.example.chat_server.filter;
 
 import com.example.chat_server.utils.JwtUtil;
+import com.example.chat_server.utils.ResultUtil;
+import com.example.chat_server.utils.UrlPermitUtil;
 import io.jsonwebtoken.Claims;
+import jakarta.annotation.Resource;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,11 +14,15 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class AuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Resource
+    private UrlPermitUtil urlPermitUtil;
 
 
     @Override
@@ -25,21 +32,28 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
             return;
         }
 
-        //todo 部分接口应该直接放行 未配置
-        String tokenName = "token";
-        String token = httpServletRequest.getHeader(tokenName);
-        Claims claims = null;
-        try {
-            claims = JwtUtil.parseToken(token);
-        } catch (Exception e) {
-            //todo 返回错误
-            return;
+        String token = httpServletRequest.getHeader("token");
+        String url = httpServletRequest.getRequestURI();
+
+        // 验证url是否需要验证
+        if (!urlPermitUtil.isPermitUrl(url)) {
+            Claims claims = null;
+            try {
+                claims = JwtUtil.parseToken(token);
+            } catch (Exception e) {
+                httpServletResponse.setContentType("application/json;charset=UTF-8");
+                httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                PrintWriter out = httpServletResponse.getWriter();
+                out.write(ResultUtil.TokenInvalid().toJSONString(0));
+                out.flush();
+                out.close();
+                return;
+            }
+            // 设置用户信息
+            Map<String, Object> map = new HashMap<>();
+            claims.entrySet().stream().forEach(e -> map.put(e.getKey(), e.getValue()));
+            httpServletRequest.setAttribute("userinfo", map);
         }
-        // 设置用户信息
-        Map<String, Object> map = new HashMap<>();
-        claims.entrySet().stream().forEach(e -> map.put(e.getKey(), e.getValue()));
-        //验证角色是否有权限
-        httpServletRequest.setAttribute("userinfo", map);
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
